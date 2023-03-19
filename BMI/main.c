05290cb6f0337a3a960f0e19e23d4d24eb869783
CimyJr <juniorqueiros45@gmail.com>
2023-03-19 16:40:23 -0300

    3rd commit

--- BMI/main.c
+++ BMI/main.c
@@ -17,13 +17,13 @@ int main(){
 	if(resultado<=18.5)
 		printf("Seu resultado e %.2f voce esta abaixo do peso", resultado);
 	
-	else if(resultado>=18.6 && resultado<=24.9 )
+	else if((resultado>=18.6) && (resultado<=24.9))
 		printf("Seu resultado e %.2f voce esta saudavel", resultado);
 		
-	else if(resultado>=25.0 && resultado<=29,9)
+	else if((resultado>=25.0) && (resultado<=29.9))
 		printf("Seu resultado e %.2f voce esta acima do peso", resultado);
 		
-	else if(resultado>=30.0 && resultado<=34.9)
+	else if((resultado>=30.0) && (resultado<=34.9))
 		printf("Seu resultado e %.2f voce esta na obesidade 1", resultado);
 	
 	
